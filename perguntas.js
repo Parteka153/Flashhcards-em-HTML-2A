c7c70dbee339caa73b6e9376ef46eecd03e3cf10
Parteka153 <joao.parteka@escola.pr.gov.br>
2025-10-10 08:04:41 -0300

    Parteka153

---
+++ perguntas.js
@@ -0,0 +1,10 @@
+criaCartao(Progamação, Oque é Python?, O Python é uma linguagem de programação)
+criaCartao('Geografia',
+     'Qual a capital da frança?',
+    'A capital da frança é Paris'
+)
+
+criaCartao('Programação',
+    'O que é uma Função?',
+    'uma função é um bloco de codigo que executa alguma tarefa'
+)
